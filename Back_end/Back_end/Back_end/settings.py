ef3af1dc82300056053176feadf30c8a9c036ae3
Muppudathi076 <varathamathavan@gmail.com>
2026-04-30 16:56:22 +0530

    sql master user session

--- Back_end/Back_end/Back_end/settings.py
+++ Back_end/Back_end/Back_end/settings.py
@@ -29,10 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^jizalyheof@*t$0^0dwz_d0k!al#vs9xy#dl5xv=d&n@vl@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['sqlmaster.onrender.com', 'localhost', '127.0.0.1', '*']
+DEBUG = False
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
